03a101f2da20548bb8d3141ee907b0779cadd368
Justin DiEmmanuele <justindiemmanuele@dhcp-wifi-8021x-168-122-6-242.bu.edu>
2021-11-03 12:06:49 -0400

    small changes to return for file reading

--- src/Utils.java
+++ src/Utils.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.io.*;
@@ -5,24 +6,31 @@ import java.io.*;
 public class Utils {
     Scanner scanner = new Scanner(System.in);
 
-    public void readTxtFile(String filename) throws IOException {
+    public ArrayList<String[]> readTxtFile(String filename) throws IOException {
         // TODO remember to use get directory and join them
+        ArrayList<String[]> data = new ArrayList<>();
         File file = new File(filename);
 
         BufferedReader br = new BufferedReader(new FileReader(file));
         String str;
 
         while ((str = br.readLine()) != null) {
-            System.out.println(str);
             String[] s = str.split("\\s+");
-            System.out.println(Arrays.toString(s));
+            data.add(s);
         }
 
+//        for (String[] line : data) {
+//            System.out.println(Arrays.toString(line));
+//        }
+
+        return data;
     }
 
     public static void main(String[] args) throws IOException {
         Utils utils = new Utils();
         utils.readTxtFile("/Users/justindiemmanuele/Documents/MS/CS611/HW3_MonstersAndHeros/Legends_Monsters_and_Heroes/Armory.txt");
+        utils.readTxtFile("/Users/justindiemmanuele/Documents/MS/CS611/HW3_MonstersAndHeros/Legends_Monsters_and_Heroes/Dragons.txt");
+        utils.readTxtFile("/Users/justindiemmanuele/Documents/MS/CS611/HW3_MonstersAndHeros/Legends_Monsters_and_Heroes/Weaponry.txt");
     }
 
 }
